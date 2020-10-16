cd874867edb9ac031a1a7b6cf9a4700eae07ba8c
Arjun Verma <averma332@gatech.edu>
2020-10-16 19:27:10 -0400

    Flag Checker

---
+++ src/flags.js
@@ -0,0 +1,33 @@
+const flagchecks = {
+    check: function(text) {
+        var ret = true;
+
+        text = text.toLowerCase();
+
+        var sentences = text.split('.');
+
+        const flags = ["believe", "think", "know"];
+
+        sentences.forEach(sentence => {
+            sentence = sentence.split(' ');
+            sentence.forEach(word => {
+                flags.forEach(flag => {
+                    if(word === flag) {
+                        console.log("This statement is lacking evidence.");
+                        ret = false;
+                        return false;
+                    }
+                });
+            });
+        });
+
+        if(ret) {
+            console.log("This statement is valid.");
+            return true;
+        }
+    }
+}
+
+flagchecks.check("I believe that Obama was a good president.");
+
+module.exports = flagchecks

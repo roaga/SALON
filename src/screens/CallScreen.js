a265dc719d08ff246fb8b10cd5c70d3e49380f06
Rohan Agarwal <ro.agarwal@hotmail.com>
2020-10-17 07:26:23 -0400

    Resolve version issue

--- src/screens/CallScreen.js
+++ src/screens/CallScreen.js
@@ -21,48 +21,42 @@ export default function CallScreen() {
         history.push('/topicview/' + topicName);
     }
 
-
     return (
         <div className="container">
             {firebase.auth().currentUser != null ?
                 <div style={{width: "100%", height: 680, minHeight: 680, overflowY: "scroll"}}>
                     <h1>Discussion on {topicName}</h1>
                     {connected ? 
-                        <div>
-                            <div style={{alignItems: "center", justifyContent: "center", display: "flex"}}>
-                                <IoMdCloseCircle class="menu-button" size={32} color={colors.primary} style={{alignSelf: "center"}} onClick={() => endCall()}/>
-                            </div>
-                            <div style={{position: "absolute", right: 0, top: 250, width: "50%", height: "60%", background: "white", borderRadius: 10, boxShadow: "0px 2px 20px grey", overflowY: "scroll"}}>
-                                <div style={{paddingBottom: "20%"}}>
-                                    {allText.map(item => {
-                                        let valid = !item.flags.isOpinion && item.flags.isSupported;
-                                        return (
-                                            <div style={{display: "flex", flexDirection: "row"}}>
-                                                <div style={{background: valid ? colors.washed : item.flags.isClaim ? colors.tertiary : colors.secondary, padding: 16, borderTopRightRadius: 10, borderBottomRightRadius: 10, boxShadow: "0px 2px 20px grey", width: 256}}>
-                                                    <h4 style={{margin: 4}}>{item.flags.isOpinion ? "Is this an opinion?" : ""}</h4>
-                                                    <h4 style={{margin: 4}}>{item.flags.isSupported ? "" : "Is this unsupported?"}</h4>
-                                                    <h4 style={{margin: 4}}>{item.flags.isClaim ? "Is the evidence factual?" : ""}</h4>
-                                                </div>
-                                                <h4 style={{marginLeft: 32, marginRight: 32, whiteSpace: "pre-line"}}>{item.text}</h4>
+                        <div style={{position: "absolute", right: 0, top: 200, width: "50%", height: "65%", background: "white", borderRadius: 10, boxShadow: "0px 2px 20px grey", overflowY: "scroll"}}>
+                            <div style={{paddingBottom: "20%"}}>
+                                {allText.map(item => {
+                                    let valid = !item.flags.isOpinion && (item.flags.isSupported || !item.flags.isClaim);
+                                    return (
+                                        <div style={{display: "flex", flexDirection: "row"}}>
+                                            <div class="App-logo-spin" style={{background: valid ? colors.washed : item.flags.isClaim ? colors.tertiary : colors.secondary, padding: 16, borderTopRightRadius: 10, borderBottomRightRadius: 10, boxShadow: "0px 2px 20px grey", width: 256, animation: valid ? "" : "App-logo-spin infinite 0.4s alternate linear"}}>
+                                                <h4 style={{margin: 4}}>{item.flags.isOpinion ? "Is this an opinion?" : ""}</h4>
+                                                <h4 style={{margin: 4}}>{item.flags.isSupported || !(item.flags.isOpinion || item.flags.isClaim) ? "" : "Is this unsupported?"}</h4>
+                                                <h4 style={{margin: 4}}>{item.flags.isClaim ? "Is the evidence factual?" : ""}</h4>
                                             </div>
-                                        );
-                                    })}
-                                </div>
-                                <form onSubmit={(e) => {
-                                    if (chatText.trim().length > 0) {
-                                        let flags = flagchecks.check(chatText);
-                                        let arr = allText;
-                                        arr.push({text: firebase.auth().currentUser.email.split("@")[0] + ": \n" + chatText, flags: flags});
-                                        setAllText(arr);
-                                    }
-                                    elementRef.current.scrollIntoView();
-                                    setChatText("");
-                                    e.preventDefault();
-                                }} style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                                    <input placeholder="Send a message..." value={chatText} onChange={event => setChatText(event.target.value)} style={{width: "45%", position: "fixed", bottom: "15%"}}/>
-                                </form>
-                                <div ref={elementRef}></div>
+                                            <h4 style={{marginLeft: 32, marginRight: 32, whiteSpace: "pre-line"}}>{item.text}</h4>
+                                        </div>
+                                    );
+                                })}
                             </div>
+                            <form onSubmit={(e) => {
+                                if (chatText.trim().length > 0) {
+                                    let flags = flagchecks.check(chatText);
+                                    let arr = allText;
+                                    arr.push({text: firebase.auth().currentUser.email.split("@")[0] + ": \n" + chatText, flags: flags});
+                                    setAllText(arr);
+                                }
+                                elementRef.current.scrollIntoView();
+                                setChatText("");
+                                e.preventDefault();
+                            }} style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                                <input placeholder="Send a message..." value={chatText} onChange={event => setChatText(event.target.value)} style={{width: "45%", position: "fixed", bottom: "15%"}}/>
+                            </form>
+                            <div ref={elementRef}></div>
                         </div>
                     : <h2 style={{textAlign: "center"}}>Searching for a salon...</h2>
                     }

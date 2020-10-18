5d59cdfece3977db5a051825f81b4cef603c94fa
Rohan Agarwal <ro.agarwal@hotmail.com>
2020-10-18 05:41:19 -0400

    Test

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+app.use(express.static('./build'));
+app.get('/', (req, res) => { 
+  res.sendFile('./build/index.html')
+});
+
 const port = process.env.PORT || 2050
 
 server.listen(port, () => {

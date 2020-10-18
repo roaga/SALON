73b8db99c4be477a90d66170e7133dfe580731d4
Arjun Verma <averma332@gatech.edu>
2020-10-18 03:02:09 -0400

    Build Production

---
+++ build/precache-manifest.d8b4bbb4f9dbdf03f3be4b1cce837bc2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bef61a17c8a8a90f0da10af0cffd8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23e13c773aa984ddd521",
+    "url": "/static/css/main.ba86ff82.chunk.css"
+  },
+  {
+    "revision": "8b5bc7c48fc2f884a753",
+    "url": "/static/js/2.3758ec7d.chunk.js"
+  },
+  {
+    "revision": "6250f5de7168d585d166546219e7aba5",
+    "url": "/static/js/2.3758ec7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e13c773aa984ddd521",
+    "url": "/static/js/main.0ae54da7.chunk.js"
+  },
+  {
+    "revision": "0ab438f8d214e9695b0c",
+    "url": "/static/js/runtime-main.b23d2ed3.js"
+  },
+  {
+    "revision": "6f8bee38cbc594315d4b81d31cc86425",
+    "url": "/static/media/Call.6f8bee38.png"
+  },
+  {
+    "revision": "43d29cd9c4da6ceb64bd94b63fc367b2",
+    "url": "/static/media/Collection.43d29cd9.png"
+  },
+  {
+    "revision": "26f25704b7807c40514fb6807c505181",
+    "url": "/static/media/ExploreTopics.26f25704.png"
+  },
+  {
+    "revision": "e16cd3bcdc66b6fea9e097b42aebbdd8",
+    "url": "/static/media/Salon.e16cd3bc.jpg"
+  },
+  {
+    "revision": "1402c28d44f0c57e7c3ca88f7b24f72e",
+    "url": "/static/media/cool-background-1.1402c28d.svg"
+  },
+  {
+    "revision": "102aa481a070f37263e207af4c989cc9",
+    "url": "/static/media/cool-background-2.102aa481.svg"
+  },
+  {
+    "revision": "0cb87c7270faed62128c7583503b9281",
+    "url": "/static/media/cool-background-3.0cb87c72.svg"
+  },
+  {
+    "revision": "404ff6a99ec35254c60021fd72c3b87d",
+    "url": "/static/media/cool-background-4.404ff6a9.svg"
+  },
+  {
+    "revision": "2a3e5a90d006634296747bcdc2aed387",
+    "url": "/static/media/cool-background.2a3e5a90.svg"
+  }
+]);
